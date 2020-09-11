3224c6e1103678b4991234ed23fe114d394673a2
meso272 <liujinyang1201@126.com>
2020-09-11 00:49:05 -0700

    dd

--- wgan_gp/wgan_gp.py
+++ wgan_gp/wgan_gp.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 
 from keras.datasets import mnist
+from keras.layers.merge import _Merge
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
@@ -8,13 +9,13 @@ from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model,load_model
 from keras.optimizers import RMSprop
 from functools import partial
+
 import keras.backend as K
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 import sys
-
-from datasets import * 
+from datasets import *
 import numpy as np
 
 class RandomWeightedAverage(_Merge):
