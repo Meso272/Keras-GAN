ff6c34abf759b7c7f5106e535fc76361f7b9777c
meso272 <liujinyang1201@126.com>
2020-09-10 23:08:00 -0700

    dd

--- wgan/wgan_test.py
+++ wgan/wgan_test.py
@@ -14,8 +14,8 @@ from sklearn.metrics import mean_squared_error
 from datasets import * 
 import numpy as np
 
-orig_test=load_CLDHGH_orig(path="/home/jliu447/lossycompression/multisnapshot-data-cleaned/CLDHGH/",startnum=50,endnum=62,scale=False)
-decomp_test=load_CLDHGH_decomp(path="/home/jliu447/lossycompression/multisnapshot-data-cleaned/CLDHGH_SZ/",startnum=50,endnum=62)
+orig_test=load_CLDHGH_orig(path="/home/jliu447/lossycompression/multisnapshot-data-cleaned/CLDHGH/",startnum=50,endnum=63,scale=False)
+decomp_test=load_CLDHGH_decomp(path="/home/jliu447/lossycompression/multisnapshot-data-cleaned/CLDHGH_SZ/",startnum=50,endnum=63)
 
 model=load_model("generator.h5")
 
